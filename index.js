672eae73332b15b4b0930101405da8664b97cba0
Russkazkin <russkazkin@gmail.com>
2022-02-04 05:56:31 +0300

    LESSON1: Add simple validation

--- index.js
+++ index.js
@@ -2,5 +2,13 @@ import colors from 'colors';
 
 const args = process.argv.slice(2);
 
-const [ test ] = args;
-console.log(`${colors.green(test)}`);
+const num1 = args[0].trim();
+const num2 = args[1].trim();
+if (typeof(num1) !== 'number' || typeof(num2) !== 'number' || num1 <= 0 || num2 <=0) {
+    console.log(`${colors.red('Введенные параметры должны быть положительными числами')}`);
+}
+const start = +num1 < +num2 ? +num1 : +num2;
+const finish = start === +num1 ? +num2 : +num1;
+
+console.log(start, 'start');
+console.log(finish, 'finish');
