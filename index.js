d2c87df1f6c2d792924ff4487b2d5ad787362b85
Russkazkin <russkazkin@gmail.com>
2022-02-08 06:01:25 +0300

    LESSON2: Add timer input

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+import moment from "moment";
+import colors from "colors";
+
 const args = process.argv.slice(2);
+
+const [t] = args;
+
+const timer = moment(t, 'DD/MM/YYYY HH:mm:ss').unix() - moment().unix();
+
+if (timer < 0) {
+    console.log(`${colors.red('Время старта должно быть в будущем.')}`);
+    process.exit(1)
+}
+
+console.log(timer);
