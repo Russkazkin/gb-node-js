73826e111095dd96d611c9a6be5c8c123f92687c
Russkazkin <russkazkin@gmail.com>
2022-02-08 07:14:04 +0300

    LESSON2: Add events

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
+import EventEmitter from 'events';
 import moment from "moment";
 import colors from "colors";
+import Timer from "./Listners/Timer.js";
 
 const args = process.argv.slice(2);
 
 const [t] = args;
 
 const time = moment(t, 'DD/MM/YYYY HH:mm:ss').unix() - moment().unix();
+
+const emitter = new EventEmitter;
+emitter.on('tick', Timer.tick);
+emitter.on('finish', Timer.timerFinishAlert);
 
 if (time < 0) {
     console.log(`${colors.red('Время старта должно быть в будущем.')}`);
@@ -15,12 +21,9 @@ if (time < 0) {
 let left = time;
 const timer = setInterval(() => {
     left--;
-    console.log(left);
+    emitter.emit('tick', left);
 }, 1000);
 setTimeout(() => {
     clearInterval(timer);
-    console.log('finish');
+    emitter.emit('finish', 'Готово!')
 }, time * 1000);
-
-
-console.log(time);
