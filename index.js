8a3c5319236f6b7bf146e36c344bc246b518c7d2
Russkazkin <russkazkin@gmail.com>
2022-02-11 06:00:39 +0300

    LESSON3: Add line reading stream

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
+import readline from 'readline';
+import fs from 'fs';
+
 import dotenv from "dotenv";
 
 dotenv.config();
+
+const readInterface = readline.createInterface({
+    input: fs.createReadStream(process.env.ACCESS_LOG),
+    output: process.stdout,
+    console: false
+});
+
+readInterface.on('line', function(line) {
+    console.log(`${line}\n`);
+});
 
