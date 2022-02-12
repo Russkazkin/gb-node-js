207a7856ed4eece7629a9ecb577d0a8cc11353c9
Russkazkin <russkazkin@gmail.com>
2022-02-12 14:45:33 +0300

    LESSON3: Add writing logic

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import readline from 'readline';
 import fs from 'fs';
-
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -11,7 +10,22 @@ const readInterface = readline.createInterface({
     console: false
 });
 
+const ips = ['89.123.1.41', '34.48.240.111'];
+const writeStreams = {};
+ips.forEach(ip => {
+    writeStreams[ip] = fs.createWriteStream(`./${ip}_requests.log`, {
+        encoding: 'utf8',
+        flags: 'a',
+    });
+});
+
 readInterface.on('line', function(line) {
-    console.log(`${line}\n`);
+    ips.forEach(ip => {
+        const testString = `^${ip}`;
+        const regexp = new RegExp(testString);
+        if (regexp.test(line)) {
+            writeStreams[ip].write(`${line}\n`);
+        }
+    });
 });
 
