2d41fa4d31c6da1c4e40eef0016927955bf254a0
Russkazkin <russkazkin@gmail.com>
2022-02-14 06:12:47 +0300

    LESSON4: Add option with yargs

--- cli.js
+++ cli.js
@@ -1,4 +1,18 @@
-#!/Users/skazkin/.nvm/versions/node/v14.18.0/bin/node
-const args = process.argv.slice(2);
+#!/usr/bin/env node
+import yargs from 'yargs';
+
+const ya = yargs(process.argv.slice(2));
+
+const options = ya
+    .usage('Usage: -p <path to file>')
+    .options('p', {
+        alias: 'path',
+        describe: 'Path to file',
+        type: 'string',
+        demandOption: true
+    })
+    .argv;
+
+console.log(options);
 
 console.log('cli');
