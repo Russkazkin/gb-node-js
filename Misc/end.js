538b727b87aa2375ef6074eecb0f7de4abae29ea
Russkazkin <russkazkin@gmail.com>
2022-02-19 07:22:10 +0300

    LESSON5: Add browsing

--- Misc/end.js
+++
@@ -1,6 +0,0 @@
-const htmlEnd = '</ul>\n' +
-    '</div>\n' +
-    '\n' +
-    '</body>\n' +
-    '</html>';
-export default htmlEnd;
--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import cluster from "cluster";
 import os from "os";
 import http from "http";
-import path from "path";
-import fs from "fs";
-import htmlStart from "./Misc/start.js";
-import htmlEnd from "./Misc/end.js";
+import * as fs from "fs";
+import * as path from "path";
+import {fileURLToPath} from 'url';
+import {htmlEnd, htmlStart} from "./Misc/htmlParts.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 if (cluster.isMaster) {
     for (let i = 0; i < os.cpus().length; i++) {
@@ -12,14 +15,40 @@ if (cluster.isMaster) {
     }
 } else {
     const server = http.createServer((req, res) => {
+        const url = req.url;
         let data = htmlStart;
-        data += '<li>Test</li>'
-        data += htmlEnd;
+        if (url === '/favicon.ico') return;
+        const currentPath = path.join(__dirname, url);
+        if (fs.lstatSync(currentPath).isFile()) {
+            fs.readFile(currentPath, 'utf8', (err, result) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log(result);
+                    data += result;
+                    data += htmlEnd;
 
-        res.writeHead(200, 'OK', {
-            'Content-Type': 'text/html',
-        });
-        res.end(data);
+                    res.writeHead(200, 'OK', {
+                        'Content-Type': 'text/html',
+                    });
+                    res.end(data);
+                }
+            });
+        } else {
+            const list = fs.readdirSync(path.join(__dirname, url));
+            data += '<ul class="browser">';
+            list.forEach(item => {
+                data += `<li><a href="http://localhost:3300${url}/${item}">${item}</a></li>`
+            });
+            data += '</ul>';
+            data += htmlEnd;
+
+            res.writeHead(200, 'OK', {
+                'Content-Type': 'text/html',
+            });
+            res.end(data);
+        }
+
     });
 
     server.listen(3300);
