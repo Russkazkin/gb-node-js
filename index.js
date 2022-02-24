e7a6e35a002e0a8c44a6ed764a3a5df0770105bb
Russkazkin <russkazkin@gmail.com>
2022-02-24 05:51:07 +0300

    LESSON6: Add http server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-const args = process.argv.slice(2);
+const socket = require('socket.io');
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+
+const server = http.createServer(((req, res) => {
+    const indexPath = path.join(__dirname, 'index.html');
+    const readStream = fs.createReadStream(indexPath);
+    readStream.pipe(res);
+}));
+
+server.listen(3300);
